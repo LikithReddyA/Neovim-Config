959e2ff565a795a3c0561cb2fcb6228833eff9e0
A Likith Reddy <alikithreddy381@gmail.com>
2022-10-14 08:50:54 +0530

    Fixed treesitter error and configured nvim-tree

--- lua/config/nvimtree.lua
+++ lua/config/nvimtree.lua
@@ -17,7 +17,7 @@ function M.setup()
 		signcolumn = "yes",
 	},
 	diagnostics = {
-		enable = false,
+		enable = true,
 		show_on_dirs = false,
 		debounce_delay = 50,
 		icons = {
--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -3,7 +3,7 @@ local M = {}
 function M.setup()
   require("nvim-treesitter.configs").setup {
     -- One of "all", "maintained" (parsers with maintainers), or a list of languages
-    ensure_installed = "maintained",
+    ensure_installed = {"c","lua","bash","dart","fish","haskell","java","javascript","markdown","python","vim","yaml"},
 
     -- Install languages synchronously (only applied to `ensure_installed`)
     sync_install = false,
